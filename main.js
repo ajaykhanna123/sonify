53427d6b34340bc1d69f8a63eae44522353df62e
ajay khanna <ajaykhanna123ak@gmail.com>
2017-11-04 18:17:54 +0530

    add datatables ,search bar and sorted songs

--- main.js
+++ main.js
@@ -117,14 +117,187 @@ window.onload=function(){
         'duration': '4:02',
         'fileName': 'song6.mp3',
         'image':'6.jpg'
+    },
+    {
+        'name': 'Jimmy_Choo_Choo_Ft_Ikka-Guri ',
+        'artist': 'Ikka-Guri',
+        'album': 'Single Track',
+        'duration': '4:02',
+        'fileName': 'song7.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'Main Tan Vi Pyar Kardan ',
+        'artist': 'HappyRaiKoti',
+        'album': 'Single Track',
+        'duration': '4:02',
+        'fileName': 'song8.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'Main Tera Boyfriend ',
+        'artist': 'Arjit Singh',
+        'album': 'Raabta',
+        'duration': '4:02',
+        'fileName': 'song9.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'Majhe Di Jatti - Kanwar Chahal ',
+        'artist': 'Kanwar Chahal',
+        'album': 'Single Track',
+        'duration': '4:02',
+        'fileName': 'song10.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'Rabb Jane - Garry Sandhu',
+        'artist': 'Garry Sandhu',
+        'album': 'Single Track',
+        'duration': '4:02',
+        'fileName': 'song11.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'Qismat ',
+        'artist': 'Ammy Virk',
+        'album': 'Qismat',
+        'duration': '4:02',
+        'fileName': 'song12.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'Mann Bharrya',
+        'artist': 'B Praak',
+        'album': 'Jaani',
+        'duration': '4:02',
+        'fileName': 'song13.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'Munda iPhone Warga ',
+        'artist': 'A-kay',
+        'album': 'Single Track',
+        'duration': '4:02',
+        'fileName': 'song14.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'One Dream ',
+        'artist': 'Babbal Rai',
+        'album': 'Dream',
+        'duration': '4:02',
+        'fileName': 'song15.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'One_Touch ',
+        'artist': '-Garry_Sandhu-Roach_Killa-',
+        'album': 'Single Track',
+        'duration': '4:02',
+        'fileName': 'song16.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'Peshi (Full Video) ',
+        'artist': ' Zora Randhawa  Latest Punjabi Song 2016  Speed Records',
+        'album': 'Single Track',
+        'duration': '4:02',
+        'fileName': 'song17.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'Na_Ja ',
+        'artist': 'Pav_Dharia',
+        'album': 'single-track',
+        'duration': '4:02',
+        'fileName': 'song18.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'Yaara ',
+        'artist': 'Sharry Mann',
+        'album': 'Rocky Mental',
+        'duration': '4:02',
+        'fileName': 'song19.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'So_High ',
+        'artist': 'Sidhu_Moose_Wala',
+        'album': 'Single Track',
+        'duration': '4:02',
+        'fileName': 'song20.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'Dil Kafiraa -   ',
+        'artist': 'Mickey Singh',
+        'album': 'Kafiraa',
+        'duration': '4:02',
+        'fileName': 'song21.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'Love Drive ',
+        'artist': 'Jimmy Kaler',
+        'album': 'Single Track',
+        'duration': '4:02',
+        'fileName': 'song22.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'Badnam -  ',
+        'artist': 'Mankirt Aulakh',
+        'album': 'Gangland',
+        'duration': '4:02',
+        'fileName': 'song23.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'LET ME LOVE YOU -  ',
+        'artist': 'Justin Bieber - ATC  Alex Goot    KHS Cover',
+        'album': 'Single Track',
+        'duration': '4:02',
+        'fileName': 'song24.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'Gangster Scene  ',
+        'artist': 'gursevek dhillon',
+        'album': 'Single Track',
+        'duration': '4:02',
+        'fileName': 'song25.mp3',
+        'image':'6.jpg'
+    },
+    {
+        'name': 'Gangland-',
+        'artist': 'Mankirt Aulakh',
+        'album': 'Gangland',
+        'duration': '4:02',
+        'fileName': 'song26.mp3',
+        'image':'6.jpg'
+    }
+    ,
+    {
+        'name': 'IK vaari aa',
+        'artist': 'Arjit Singh',
+        'album': 'Raabta',
+        'duration': '4:02',
+        'fileName': 'song27.mp3',
+        'image':'27.jpg'
     }
 
   ]
+
+
   changeCurrentSongDetails(songs[0]);
   function changeCurrentSongDetails(songObj) {
     $('.current-song-image').attr('src','img/' + songObj.image)
     $('.current-song-name').text(songObj.name)
+    $('.current-song-artist').text(songObj.artist)
     $('.current-song-album').text(songObj.album)
+    $('.current-song-duration').text(songObj.duration)
 }
 
 
@@ -158,4 +331,7 @@ window.onload=function(){
     song.find('.song-length').text(obj.duration);
     addSongNameClickEvent(obj,i+1);
 }
+$('#songs').DataTable({
+      paging: true
+  });
 }
